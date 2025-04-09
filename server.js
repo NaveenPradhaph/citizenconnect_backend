6010686d5b38bf6db26e67555b439d694621e113
NaveenPradhaph <pradhaphnaveen@gmail.com>
2025-04-09 12:08:04 +0530

    vercel

--- server.js
+++ server.js
@@ -84,8 +84,12 @@ app.use("/api/petition", viewRoutes);
 //     });
 //   }
 // });
+
+
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// });
+
+module.exports = app;
